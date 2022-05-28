aa8e3858bfcd0418b95f18d1e3d3acf08a6231f5
özcan çetin <cetin445@gmail.com>
2022-05-28 18:02:02 +0300

    js v2

--- app.js
+++ app.js
@@ -1,5 +1,26 @@
 const minus = document.querySelectorAll(".left")
 const plus = document.querySelectorAll(".right")
 
+plus.forEach((item)=>{
+    console.log(item);
+    item.addEventListener("click", add)
+})
+
+function add(item){
+    let amount = this.previousElementSibling
+    amount.innerHTML = Number(amount.innerHTML) + 1
+  }
+
+minus.forEach((item)=>{
+    console.log(item);
+    item.addEventListener("click", substract)
+})
+
+function substract(item){
+    let amount = this.nextElementSibling
+    if(Number(amount.innerHTML) > 0){
+    amount.innerHTML = Number(amount.innerHTML) - 1
+}
+  }
 
 console.log(plus[0].innerHTML);
