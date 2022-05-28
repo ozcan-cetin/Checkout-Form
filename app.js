9253d69e6f87f1bdc8c1fc327fe1750ecf5c07d5
özcan çetin <cetin445@gmail.com>
2022-05-28 23:13:19 +0300

    v4 total price completed

--- app.js
+++ app.js
@@ -3,32 +3,58 @@
 const minus = document.querySelectorAll(".left")
 const plus = document.querySelectorAll(".right")
 
-//! I determined the amount of product using a function.
+//! ***********************
+
+const total = document.getElementById("total")
+console.log(total.innerHTML);
+
+
+//? I determined the amount of product using a function.
 
 // INCREASE QUANTITY
 
 plus.forEach((item)=>{
-    console.log(item);
+    // console.log(item);
     item.addEventListener("click", add)
+    
+// SET PRICE   
+    
+    // item.addEventListener("click", priceup)
 })
 
 function add(item){
     let amount = this.previousElementSibling
     amount.innerHTML = Number(amount.innerHTML) + 1
+
+    total.innerHTML ="$" + (parseFloat(total.innerHTML.slice(1))+ parseFloat(this.parentElement.previousElementSibling.firstElementChild.innerHTML.slice(1))).toFixed(2)
   }
+
+console.log(plus[0].parentElement.previousElementSibling.firstElementChild.innerHTML.slice(1));
 
   // REDUCE THE QUANTITY
 
 minus.forEach((item)=>{
-    console.log(item);
+    // console.log(item);
     item.addEventListener("click", substract)
+
+    // SET PRICE 
+
+    // item.addEventListener("click", pricedown)
 })
 
 function substract(item){
     let amount = this.nextElementSibling
     if(Number(amount.innerHTML) > 0){
     amount.innerHTML = Number(amount.innerHTML) - 1
+
+
+    total.innerHTML ="$" + (parseFloat(total.innerHTML.slice(1)) - parseFloat(this.parentElement.previousElementSibling.firstElementChild.innerHTML.slice(1))).toFixed(2)
+    
 }
   }
+
+
+
+
 
 console.log(plus[0].innerHTML);
