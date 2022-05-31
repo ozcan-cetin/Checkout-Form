25713dcacb9183e930e53580d0d7501fb688aad5
özcan çetin <cetin445@gmail.com>
2022-05-31 12:45:39 +0300

    closest method added

--- app.js
+++ app.js
@@ -25,10 +25,18 @@ plus.forEach((item)=>{
 
 function add(item){
     let amount = this.previousElementSibling
+
     amount.innerHTML = Number(amount.innerHTML) + 1
 
-    total.innerHTML ="$" + (parseFloat(total.innerHTML.slice(1))+ parseFloat(this.parentElement.previousElementSibling.firstElementChild.innerHTML.slice(1))).toFixed(2)
+    const product_price = this.closest(".display").querySelector("span").innerHTML.slice(1)
+
+    // total.innerHTML ="$" + (parseFloat(total.innerHTML.slice(1))+ parseFloat(this.parentElement.previousElementSibling.firstElementChild.innerHTML.slice(1))).toFixed(2)
+
+    total.innerHTML ="$" + (parseFloat(total.innerHTML.slice(1))+ parseFloat(product_price)).toFixed(2)
   }
+
+//   const product_price = plus[0].closest(".display").querySelector("span").innerHTML
+//   console.log(product_price);
 
 
   // REDUCE THE QUANTITY AND TOTAL PRICE
@@ -41,11 +49,16 @@ minus.forEach((item)=>{
 
 function substract(item){
     let amount = this.nextElementSibling
-    if(Number(amount.innerHTML) > 0){
+
+    if(Number(amount.innerHTML) > 1){
     amount.innerHTML = Number(amount.innerHTML) - 1
 
+    const product_price = this.closest(".display").querySelector("span").innerHTML.slice(1)
 
-    total.innerHTML ="$" + (parseFloat(total.innerHTML.slice(1)) - parseFloat(this.parentElement.previousElementSibling.firstElementChild.innerHTML.slice(1))).toFixed(2)
+
+    // total.innerHTML ="$" + (parseFloat(total.innerHTML.slice(1)) - parseFloat(this.parentElement.previousElementSibling.firstElementChild.innerHTML.slice(1))).toFixed(2)
+
+    total.innerHTML ="$" + (parseFloat(total.innerHTML.slice(1)) - parseFloat(product_price)).toFixed(2)
     
 }
   }
@@ -58,8 +71,16 @@ remove.forEach((item)=>{
 function delet(item){
     this.parentElement.parentElement.style.display = "none";
 
-    total.innerHTML ="$" + (parseFloat(total.innerHTML.slice(1)) - (Number(this.previousElementSibling.firstElementChild.nextElementSibling.innerHTML) * parseFloat(this.previousElementSibling.previousElementSibling.firstElementChild.innerHTML.slice(1)))).toFixed(2)
+    const product_price = this.closest(".display").querySelector("span").innerHTML.slice(1)
+
+    const product_amount = this.closest(".display").querySelector("label").innerHTML
+
+    // total.innerHTML ="$" + (parseFloat(total.innerHTML.slice(1)) - (Number(this.previousElementSibling.firstElementChild.nextElementSibling.innerHTML) * parseFloat(this.previousElementSibling.previousElementSibling.firstElementChild.innerHTML.slice(1)))).toFixed(2)
+
+    total.innerHTML ="$" + (parseFloat(total.innerHTML.slice(1)) - (Number(product_amount) * parseFloat(product_price))).toFixed(2)
 }
+// const product_amount = plus[0].closest(".display").querySelector("label").innerHTML
+// console.log(product_amount);
 
 
 
